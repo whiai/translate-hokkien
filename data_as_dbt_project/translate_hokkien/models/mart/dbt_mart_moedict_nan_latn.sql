5425838470a8ebd090deea54bf812a96f1fa1ff0
W <hanley@infuseai.io>
2023-11-02 12:06:29 +1100

    Updated NAN_LATN Random Generation Script to use Random Seed
    (This seems to fix the issue of it not being properly random before.)

--- data_as_dbt_project/translate_hokkien/models/mart/dbt_mart_moedict_nan_latn.sql
+++ data_as_dbt_project/translate_hokkien/models/mart/dbt_mart_moedict_nan_latn.sql
@@ -1,7 +1,11 @@
-with moedict_joined as (
-    select *,
-        random() as r
-    from {{ ref('dbt_mart_moedict_joined') }}
+with moedict_with_rand as (
+    select 
+        m.*,
+        r.value as rand
+    from 
+        {{ ref('dbt_mart_moedict_joined') }} m
+    join
+        {{ ref('dbt_stg_rand_30000') }} r on m.sentence_id = r.id
 )
 
 select
@@ -10,18 +14,18 @@ select
     zh_xc,
     en,
     case
-        when r<0.4 then nan_tailo
-        when r<0.47 then taibun_tl_south
-        when r<0.5 then taibun_tl_north
-        when r<0.85 then taibun_poj_south
+        when r<.40 then nan_tailo
+        when r<.47 then taibun_tl_south
+        when r<.50 then taibun_tl_north
+        when r<.85 then taibun_poj_south
         else taibun_poj_north
     end as nan_latn,
     case
-        when r<0.4 then 'nan_tailo'
-        when r<0.47 then 'taibun_tl_south'
-        when r<0.5 then 'taibun_tl_north'
-        when r<0.85 then 'taibun_poj_south'
+        when r<.40 then 'nan_tailo'
+        when r<.47 then 'taibun_tl_south'
+        when r<.50 then 'taibun_tl_north'
+        when r<.85 then 'taibun_poj_south'
         else 'taibun_poj_north'
     end as nan_latn_type,
-    r
-from moedict_joined
+    rand
+from moedict_with_rand
